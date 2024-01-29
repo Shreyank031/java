2cdfd1fcf6772f60c8c8188d17190286b49cd3c6
Shreyank031 <shreyankchandu@gmail.com>
2024-01-29 09:37:08 +0530

    updated

--- programs/random/stch.java
+++ programs/random/stch.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 // Import Scanner class to read user input
 
-public class swtch {
+public class stch {
     // Main method
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in); // Create a Scanner object to read user input
@@ -15,6 +15,13 @@ public class swtch {
             case 1, 2, 3, 4, 5 -> System.out.println("Weekdays"); // If dayNum is 1, 2, 3, 4, or 5, print "Weekdays"
             case 6, 7 -> System.out.println("Weekends"); // If dayNum is 6 or 7, print "Weekends"
             default -> System.out.println("Invalid day number"); // If dayNum doesn't match any case, print "Invalid day number"
+																	
+			//			In kotlin we use "when" which is similar to using switch in java"
+//			when(dayNum) {
+//				12 -> println("It is number twelve")
+//				"dog" -> println("The animal is a dog")
+//				else -> println("Neither number nor a animal")
+
         }
     }
 }
